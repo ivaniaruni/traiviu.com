c8e0dc918ff672901ecb5e5cb54ba08345a76870
Ivan Iarunichev <iarunichevivan@gmail.com>
2025-03-08 10:50:35 +0100

    traiviu.com - 3.08.202502

--- trailer-block-games.js
+++ trailer-block-games.js
@@ -14,7 +14,7 @@ const modalRating = document.getElementById("modal-rating");
 // Series information (expand this list as needed)
 const seriesInfo = {
     // Upcoming Series
-    "Andor":{
+    "Capcom Fighting Collection 2":{
         title: "Andor: A Star Wars Story",
         description: "The final season follows Cassian Andor’s journey from thief to rebel spy, leading directly into Rogue One: A Star Wars Story.",
         directors: "Ariel Kleiman, Janus Metz, Alonso Ruizpalacios",
@@ -25,7 +25,7 @@ const seriesInfo = {
         releaseDate: "April 2025",
         rating: "TBD"
     },
-    "Devil May Cry":{
+    "Doom The Dark Ages":{
         title: "Devil May Cry",
         description: "An animated adaptation of the popular Capcom video game franchise, following the adventures of Dante, a demon hunter who battles supernatural forces to protect humanity.",
         directors: "Adi Shankar",
@@ -36,7 +36,7 @@ const seriesInfo = {
         releaseDate: " April 3, 2025",
         rating: "TBD"
     },
-    "The Eternaut":{
+    "Dune Awakening":{
         title: "The Eternaut",
         description: "Based on the classic Argentinian comic, the series follows a group of survivors in Buenos Aires facing an alien invasion and deadly weather phenomena.",
         directors: "Bruno Stagnaro",
@@ -47,7 +47,7 @@ const seriesInfo = {
         releaseDate: "April 2025",
         rating: "TBD"
     },
-    "The Last Of Us 2":{
+    "Fantasy Life i the Girl Who Steals Time":{
         title: "The Last of Us - Season 2",
         description: "The second season of The Last of Us continues the story of Ellie and Joel in a post-apocalyptic world, introducing new characters and exploring themes of revenge and redemption.",
         directors: "Craig Mazin, Neil Druckmann",
@@ -58,7 +58,7 @@ const seriesInfo = {
         releaseDate: "April 2025",
         rating: "TBD"
     },
-    "You":{
+    "Metal Eden":{
         title: "You - Season 5",
         description: "The fifth season follows Joe Goldberg’s obsessions and dangerous relationships as he navigates new cities and faces the consequences of his past.",
         directors: "Various",
@@ -69,7 +69,7 @@ const seriesInfo = {
         releaseDate: "April 24, 2025",
         rating: "TBD"
     },
-    "How To Sell Drugs Online Fast":{
+    "The Midnight Walk":{
         title: "How To Sell Drugs Online - Season 4",
         description: "The story of Moritz continues as he deals with new challenges in his personal and criminal life while running an online drug empire.",
         directors: "Arne Feldhusen, Lars Montag",
@@ -81,7 +81,7 @@ const seriesInfo = {
         rating: "TBD"
     },
     // Trending Series
-    "9-1-1":{
+    "Clair Obscur Expedition 33":{
         title: "9-1-1 - Season 8 (Part 2)",
         description: "A high-intensity drama following first responders as they tackle life-threatening emergencies while dealing with their personal struggles.",
         directors: "Bradley Buecker, Gwyneth Horder-Payton, Jennifer Lynch, among others.",
@@ -92,7 +92,7 @@ const seriesInfo = {
         releaseDate: "March 6, 2025",
         rating: "7.9/10"
     },
-    "Alert Missing Persons Unit":{
+    "Days Gone Remastered":{
         title: "Alert Missing Persons Unit - Season 3",
         description: "A gripping procedural drama that follows a specialized team solving the most complex missing persons cases while uncovering personal mysteries.",
         directors: "Adam Kane, Bola Ogun, Milan Cheylov, among others.",
@@ -103,7 +103,7 @@ const seriesInfo = {
         releaseDate: "March 25, 2025",
         rating: "7.1/10"
     },
-    "Bosch Legacy":{
+    "Elden Ring Nightreign":{
         title: "Bosch: Legacy - Season 3",
         description: "The critically acclaimed series continues as Harry Bosch takes on new cases, navigating corruption, justice, and his own personal demons.",
         directors: "Patrick Cady, Alex Zakrzewski, Sharat Raju, among others.",
@@ -114,7 +114,7 @@ const seriesInfo = {
         releaseDate: "March 27, 2025",
         rating: "8.9/10"
     },
-    "Daredevil Born Again":{
+    "Fatal Fury City of the Wolves":{
         title: "Daredevil Born Again",
         description: "A thrilling continuation of the iconic Daredevil saga, following Matt Murdock as he faces new enemies and moral dilemmas in Hell’s Kitchen.",
         directors: "Michael Cuesta, Jeffrey Nachmanoff, Clark Johnson",
@@ -125,7 +125,7 @@ const seriesInfo = {
         releaseDate: "TBA 2025",
         rating: "8.6/10"
     },
-    "Formula 1 Drive to Survive":{
+    "Five Nights at Freddys Secret of the Mimic":{
         title: "Formula 1: Drive to Survive - Season 7",
         description: "An adrenaline-fueled docuseries that goes behind the scenes of the Formula 1 World Championship, capturing the drama, rivalries, and triumphs of the sport.",
         directors: "Martin Webb, Nick Hardie, James Routh",
@@ -136,7 +136,7 @@ const seriesInfo = {
         releaseDate: "March 8, 2025",
         rating: "8.5/10"
     },
-    "Gangs of London":{
+    "Lost Records Bloom and Rage Tape 2":{
         title: "Gangs of London - Season 3",
         description: "A gritty crime drama that explores the brutal power struggles within London’s criminal underworld.",
         directors: "Corin Hardy, Marcela Said, Nima Nourizadeh, among others.",
@@ -149,7 +149,7 @@ const seriesInfo = {
     },
 
     // Second Part Trending
-    "1923":{
+    "Lost Soul Aside":{
         title: "1923: Season 2",
         description: "A captivating drama series that continues the saga of the Dutton family as they face challenges in the early 20th century, including Westward expansion, Prohibition, and the Great Depression.",
         directors: "Ben Richardson, Guy Ferland, Stephen Kay, Christina Voros",
@@ -160,7 +160,7 @@ const seriesInfo = {
         releaseDate: "March 10, 2025",
         rating: "8.7/10"
     },
-    "Power Book III Raising Kanan":{
+    "Lushfoil Photography Sim":{
         title: "Power Book III: Raising Kanan - Season 4",
         description: "A gripping prequel set in the 1990s, chronicling the early life of Kanan Stark as he delves into the drug trade, exploring themes of family, loyalty, and ambition.",
         directors: "Rob Hardy, Eif Rivera, Hernán Otaño",
@@ -171,7 +171,7 @@ const seriesInfo = {
         releaseDate: "March 7, 2025",
         rating: "7.7/10"
     },
-    "The Residence":{
+    "Rune Factory Guardians of Azuma":{
         title: "The Residence",
         description: "A captivating mystery series set within the walls of the White House, where a detective investigates a scandalous murder, uncovering secrets that could shake the nation.",
         directors: "Liza Johnson, Dee Rees",
@@ -182,7 +182,7 @@ const seriesInfo = {
         releaseDate: "March 20, 2025",
         rating: "TBA"
     },
-    "The Righteous Gemstones": {
+    "South of Midnight": {
         title: "The Righteous Gemstones - Season 4",
         description: "A dark comedy following a world-famous televangelist family with a long tradition of deviance, greed, and charitable work, as they navigate internal conflicts and external threats.",
         directors: "Danny McBride, Jody Hill, David Gordon Green",
@@ -193,7 +193,7 @@ const seriesInfo = {
         releaseDate: "March 9, 2025",
         rating: "8.1/10"
     },
-    "The Studio": {
+    "The Last of Us Part II Remastered": {
         title: "The Studio",
         description: "An inspiring drama set in the high-pressure world of a creative agency, focusing on the stories of its talented yet conflicted employees.",
         directors: "Various Talents",
@@ -204,7 +204,7 @@ const seriesInfo = {
         releaseDate: "March 26, 2025",
         rating: "TBA"
     },
-    "The Wheel of Time": {
+    "Tony Hawks Pro Skater 3+4": {
         title: "The Wheel of Time - Season 3",
         description: "An epic fantasy series that follows Moiraine, a member of a powerful all-female organization, as she embarks on a dangerous journey with five young villagers, believing one of them is the reincarnation of the Dragon, destined to save or destroy the world.",
         directors: "Uta Briesewitz, Salli Richardson-Whitfield, Wayne Yip",
@@ -217,7 +217,7 @@ const seriesInfo = {
     },
 
     // Missed Series
-    "Survival of the Thickest":{
+    "Directive 8020 A Dark Pictures Game":{
         title: "Survival of the Thickest Season 2",
         description: "A heartwarming comedy-drama following Mavis Beaumont, a plus-size stylist navigating life's challenges and embracing self-love after a transformative breakup.",
         directors: "Kim Nguyen, Tasha Smith, Thembi Banks, Amy Aniobi",
@@ -228,7 +228,7 @@ const seriesInfo = {
         releaseDate: "March 27, 2025",
         rating: "N/A"
     },
-    "Dark Winds":{
+    "Hell Is Us":{
         title: "Dark Winds: Season 3",
         description: "A gripping psychological thriller set in the 1970s Southwest, following two Navajo police officers as they confront their spiritual beliefs while investigating mysterious crimes.",
         directors: "Various Talents",
@@ -239,7 +239,7 @@ const seriesInfo = {
         releaseDate: "March 9, 2025",
         rating: "7.6/10"
     },
-    "Happy Face":{
+    "Metal Gear Solid Δ Snake Eater":{
         title: "Happy Face",
         description: "n intense true crime drama inspired by the real-life story of Melissa G. Moore, who, at 15, discovered her father was the notorious, Happy Face Killer. The series delves into her journey of grappling with this revelation and its aftermath.",
         directors: "Michael Showalter",
@@ -250,7 +250,7 @@ const seriesInfo = {
         releaseDate: "March 20, 2025",
         rating: "N/A"
     },
-    "Ludwig":{
+    "Shinobi Art of Vengeance":{
         title: "Ludwig",
         description: "A clever detective comedy that follows John 'Ludwig' Taylor, a reclusive puzzle setter who assumes his missing twin brother's identity, leading to unexpected adventures in crime-solving.",
         directors: "Robert McKillop, Jill Robertson",
@@ -261,7 +261,7 @@ const seriesInfo = {
         releaseDate: "March 20, 2025",
         rating: "N/A"
     },
-    "The Leopard":{
+    "Tales of the Shire A Lord of the Rings Game":{
         title: "The Leopard",
         description: "A captivating historical drama based on the classic novel Il Gattopardo, exploring power, tradition, and change in 19th-century Italy.",
         directors: "Tom Shankland, Giuseppe Capotondi, Laura Luchetti",
